18ea64835355883e8d350f6f376dfb195cf414ea
Chinmayi-H-M <hmschinmayi06@gmail.com>
2025-10-07 18:39:48 +0530

    Remove button in the cart

--- cart.js
+++ cart.js
@@ -1,21 +1,39 @@
 const cartContainer=document.querySelector(".cart-container");
 
-const cart=JSON.parse(localStorage.getItem("cart")) || [];
+let cart=JSON.parse(localStorage.getItem("cart")) || [];
+function renderCart(){
+    cartContainer.innerHTML="";
 
 if(cart.length===0){
-    cartContainer.innerHTML="<p>Your cart is empty.</p>";  
+    cartContainer.innerHTML="<p>Your cart is empty.</p>"; 
+    return; 
 }
-else{
-    cart.forEach(item => {
-        const div= document.createElement("div");
-        div.classList.add(".cart-item");
-        div.innerHTML=`
-        <img src=${item.image} alt=""
-                    class="image"/>
-                    <h1 class="name">${item.title}</h1>
-                    <p class="cart-amount">₹ ${item.amount}</p>
-                    <button class="buy-btn cart-btn-page">Buy Now</button>
+cart.forEach((item, index) => {
+    const div= document.createElement("div");
+    div.classList.add("cart-item");
+    div.innerHTML=`
+    <img src=${item.image} alt=""
+        class="image"/>
+    <h1 class="name-cart">${item.title}</h1>
+    <p class="cart-amount">₹ ${item.amount}</p>
+    <div class="cart-btns">
+    <button class="buy-btn cart-btn-page">Buy Now</button>
+    <button class="buy-btn remove-btn" data-index="${index}">Remove</button>
+    </div>
         `;
         cartContainer.appendChild(div);
     });
 }
+
+// Listen for remove button clicks
+
+document.addEventListener("click", function(e){
+    if (e.target.classList.contains("remove-btn")){
+        const index=e.target.getAttribute("data-index");
+        cart.splice(index,1);
+        localStorage.setItem("cart", JSON.stringify(cart));
+        renderCart();
+    }
+});
+
+renderCart();
